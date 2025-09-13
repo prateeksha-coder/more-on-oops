b79611c6827432781fe6da3d48ea2950b7215cc3
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-09-13 18:48:02 +0530

    commit

--- a3.py
+++ a3.py
@@ -10,7 +10,6 @@ class pair_elements:
 				return (lookup[target - num], i )
 			lookup[num] = i
 
-# take input of dum from the user
 value = int(input("Enter sum for which you want to make this search : "))
 print(pair_elements().twoSum((10,20,30,40,50,60,70),value))
 #print("index1=%d, index2=%d" % pair_elements().twoSum((10,20,30,40,50,60,70),value))
